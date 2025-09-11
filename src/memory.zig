ec23d74f1d0015dd0be27c9a728ab4860af37e28
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2025-09-11 15:36:24 +0300

    Use usize memory offsets for portability

--- src/memory.zig
+++ src/memory.zig
@@ -13,7 +13,7 @@ pub fn init(gpa: std.mem.Allocator) !Memory {
     };
 }
 
-pub fn slice(self: *Memory, start: u64, size: u64) []u8 {
+pub fn slice(self: *Memory, start: usize, size: usize) []u8 {
     const sliceSize = @min(self.buf.len - start, size);
     return self.buf[start .. start + sliceSize];
 }
@@ -24,13 +24,13 @@ pub fn growToFit(self: *Memory, offset: u256, size: u256, availableGas: i32) !i3
     if (size == 0) {
         return 0;
     }
-    if (offset > std.math.maxInt(u64) or size > std.math.maxInt(u64)) {
+    if (offset > std.math.maxInt(usize) or size > std.math.maxInt(usize)) {
         return evm.Errors.GasOverflow;
     }
 
-    const offset64: u64 = @intCast(offset);
-    const size64: u64 = @intCast(size);
-    const memSize, const overflown = @addWithOverflow(offset64, size64);
+    const off: usize = @intCast(offset);
+    const sz: usize = @intCast(size);
+    const memSize, const overflown = @addWithOverflow(off, sz);
     if (overflown == 1) {
         return evm.Errors.GasOverflow;
     }
@@ -49,7 +49,7 @@ pub fn growToFit(self: *Memory, offset: u256, size: u256, availableGas: i32) !i3
 
 // Copies from source to the memory region given. Clears tail part of the memory region
 // that wasn't filled if any
-pub fn copyAndClearRemaining(self: *Memory, offset: u64, size: u64, source: []const u8) void {
+pub fn copyAndClearRemaining(self: *Memory, offset: usize, size: usize, source: []const u8) void {
     const destination = self.slice(offset, size);
     @memcpy(destination[0..source.len], source);
     @memset(destination[source.len..], 0);
