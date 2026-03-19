05d28b0f542e1fb576b64a806a5c032c46a6b52a
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2026-03-19 23:49:27 +0300

    fix: BYTE shift overflow

--- src/ops.zig
+++ src/ops.zig
@@ -194,7 +194,7 @@ pub fn Ops(comptime spec: Spec) type {
             if (args[1] > 31) {
                 args[0] = 0;
             } else {
-                const index: u5 = @intCast(args[1]);
+                const index: u8 = @intCast(args[1]);
                 args[0] = @as(u8, @truncate(args[0] >> (index * 8)));
             }
             return next(next_ip, gas - spec.constantGas(.BYTE), new_stack_head, frame);
