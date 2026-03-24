907ccddc8c0db9ff01321b655089baadde7f6918
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2026-03-24 20:58:40 +0300

    feat: impl BALANCE dynamic gas

--- src/ops.zig
+++ src/ops.zig
@@ -309,8 +309,11 @@ pub fn Ops(comptime spec: Spec) type {
 
         pub fn balance(next_ip: InstructionPointer, gas: i32, stack_head: u16, frame: *evm.Frame) evm.Errors!void {
             const new_stack_head, const args = try frame.stackPop(stack_head, 1, 1);
-            args[0] = frame.state.accounts.read(@truncate(args[0])).balance;
-            return next(next_ip, gas - spec.constantGas(.BALANCE), new_stack_head, frame);
+            const target: u160 = @truncate(args[0]);
+            const is_warm = frame.evm.accessAccount(target);
+            const dynamic_cost: i32 = if (is_warm) 100 else 2600;
+            args[0] = frame.state.accounts.read(target).balance;
+            return next(next_ip, gas - spec.constantGas(.BALANCE) - dynamic_cost, new_stack_head, frame);
         }
 
         pub fn origin(next_ip: InstructionPointer, gas: i32, stack_head: u16, frame: *evm.Frame) evm.Errors!void {
