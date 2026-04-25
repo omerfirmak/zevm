b4c492eecc061818502d0fde72dac888c7136f47
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2026-04-25 14:45:05 +0300

    feat: introduce WIP block processor

--- src/evm/config.zig
+++ src/evm/config.zig
@@ -1,4 +1,4 @@
 pub const Config = struct {
     fork: @import("spec.zig").Spec,
-    tracing_enabled: bool,
+    tracing_enabled: bool = false,
 };
--- src/root.zig
+++ src/root.zig
@@ -6,4 +6,8 @@ pub const Bytecode = @import("./evm/bytecode.zig").Bytecode;
 pub const StorageTrie = @import("./trie/storage_trie.zig").StorageTrie;
 pub const AccountTrie = @import("./trie/account_trie.zig").AccountTrie;
 
+pub const chainspec = @import("./processor/chainspec.zig");
+pub const processor = @import("./processor/processor.zig");
+
 pub const types = @import("types");
+pub const Fork = @import("forks.zig").Fork;
