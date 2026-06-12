7cc3199114ec51f67f593511710e79cf011a8d4e
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2026-06-12 14:57:07 +0300

    feat: guest compilation for zisk

---
+++ src/stateless/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const zkvm = @import("zkvm");
+const verify_ssz = @import("guest.zig").verify_ssz;
+
+extern const _kernel_heap_bottom: u8;
+extern const _kernel_heap_size: u8;
+
+comptime {
+    @export(&zkvmMain, .{ .name = "main" });
+}
+
+fn zkvmMain() callconv(.c) void {
+    const heap_ptr: [*]u8 = @ptrCast(@constCast(&_kernel_heap_bottom));
+    const heap_len: usize = @intFromPtr(&_kernel_heap_size);
+    var fba = std.heap.FixedBufferAllocator.init(heap_ptr[0..heap_len]);
+
+    var input_ptr: [*c]const u8 = undefined;
+    var input_len: usize = undefined;
+    zkvm.read_input(&input_ptr, &input_len);
+
+    const output = verify_ssz(fba.allocator(), input_ptr[0..input_len]) catch
+        @panic("verify_ssz failed");
+    zkvm.write_output(output.ptr, output.len);
+}
