27b23670b4c8c22d83af27842d6f02676fd5fe8c
Ömer Faruk IRMAK <omerfirmak@gmail.com>
2026-07-31 00:12:51 +0300

    feat: implement forkid calculation

--- src/unit_tests.zig
+++ src/unit_tests.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 comptime {
     _ = @import("types.zig");
+    _ = @import("forks.zig");
     _ = @import("trie/trie.zig");
     _ = @import("evm/crypto/ripemd160.zig");
 }
